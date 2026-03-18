f4b986057755c2a2c81967d4f8b7bb5619c2ebea
BorysAugustyniak <293169@student.pwr.edu.pl>
2026-03-18 16:43:57 +0100

    Dodano l1/z2a

---
+++ l1/z2/293169_z2a.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int nwd(int a, int b){
+	if (a == 0 || b == 0){
+		return max(a, b);
+	}
+	//a < b zawsze w naszym przypadku
+	return nwd(b % a, a);
+}
+
+int tocjent(int n){
+	int toc = 0;
+	for (int i = 1; i < n; ++i){
+		if (nwd(i, n) == 1){
+			++toc;
+		}
+	}
+	return toc;
+}
+
+int main(){
+	int n;
+	cin >> n;
+	cout << tocjent(n);
+}
