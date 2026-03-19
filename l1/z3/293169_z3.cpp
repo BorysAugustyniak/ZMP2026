67c52b64ecf2b377e18e0b98d307f3bfedc9b230
BorysAugustyniak <293169@student.pwr.edu.pl>
2026-03-19 03:14:14 +0100

    Poprawiono l1/z3

--- l1/z3/293169_z3.cpp
+++ l1/z3/293169_z3.cpp
@@ -11,12 +11,12 @@ int main(){
 	//zmienna do zapisania 2 ^ k lub 5 ^ k
 	int poteg = 2;
 
-	while (poteg < n){
+	while (poteg <= n){
 		vp2 += (n / poteg);
 		poteg *= 2;
 	}
 	poteg = 5;
-	while (poteg < n){
+	while (poteg <= n){
 		vp5 += (n / poteg);
 		poteg *= 5;
 	}
