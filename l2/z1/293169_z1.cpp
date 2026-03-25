d232bc4cebb8cc07d186017e2f8b557f0ea2e73f
BorysAugustyniak <293169@student.pwr.edu.pl>
2026-03-25 15:43:05 +0100

    Dodano l2/z1

---
+++ l2/z1/293169_z1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int nwd(int a, int b){
+	if (a == 0 || b == 0){
+		return max(a, b);
+	}
+	//a < b zawsze w naszym przypadku
+	return nwd(b % a, a);
+}
+
+int main(){
+	int n , NWD;
+	cin >> n;
+	int* tab = new int[n];
+	for (int i = 0; i < n; ++i){
+		cin >> tab[i];
+	}
+	NWD = tab[0];
+	for (int i = 1; i < n; ++i){
+		NWD = nwd(NWD, tab[i]);
+	}
+	cout << NWD;
+}
